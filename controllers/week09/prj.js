59377fbe513bac194e93edc5436d55ae2af82f4a
Matthew Reed <matthew.reed2000@gmail.com>
2020-03-07 13:40:12 -0700

    Started adding some display logic

--- controllers/week09/prj.js
+++ controllers/week09/prj.js
@@ -1,6 +1,6 @@
 function run(req, res) {
   if (req.query):
-    res.render('pages/week09/calculator');
+    res.render('pages/week09/calculator', {error:false});
   else:
     res.render('pages/week09/calculator', {error: true});
 }
